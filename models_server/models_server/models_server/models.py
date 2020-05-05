5fbdc3d410290c49dc3ad8725f6b4936631dd58f
Артем Шабунин <artemsabunin@MacBook-Pro-Artem.local>
2020-05-05 22:16:34 +0300

    Добавлен FMU, изменена база данных

--- models_server/models_server/models_server/models.py
+++ models_server/models_server/models_server/models.py
@@ -13,9 +13,20 @@ class User(db.Model):
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    model_id = db.Column(db.Integer, db.ForeignKey('model.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Task {}>'.format(self.body)
+
+class Model(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True, unique=True)
+    tasks = db.relationship('Task', backref='model', lazy='dynamic')
+
+#class Variable(db.Model):
+
+#class Achive(db.Model):
+
+#class Timeseries(db.Model):
