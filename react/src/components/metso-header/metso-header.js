824975bf47a7f8c08affc8859c9ada2a7db667fd
Артем Шабунин <artemsabunin@MacBook-Pro-Artem.local>
2021-01-10 21:11:41 +0300

    Работа над metso-header

--- react/src/components/metso-header/metso-header.js
+++ react/src/components/metso-header/metso-header.js
@@ -9,54 +9,83 @@ const MetsoHeader = () => {
         <div className="left">
         </div>
 
-        <div className="btn-pannel">
+        <div className="container-fluid">
           <div className="row">
-            <div class="col">
-              <button class="btn btn-primary btn-block">Boiler</button>
-            </div>
-            <div class="col">
-              <button class="btn btn-primary btn-block">Turbine</button>
-            </div>
-            <div class="col">
-              <button class="btn btn-primary btn-block">FW Pump</button>
-            </div>
-          </div>
+            <div className="col">
 
-          <div className="row">
-            <div class="col">
-              <button class="btn btn-primary btn-block">Boiler</button>
-            </div>
-            <div class="col">
-              <button class="btn btn-primary btn-block">Turbine</button>
-            </div>
-            <div class="col">
-              <button class="btn btn-primary btn-block">FW Pump</button>
-            </div>
-          </div>
+              <div className="btn-pannel">
+                <div className="row">
+                  <div className="col">
+                    <button className="btn btn-primary btn-block">Boiler</button>
+                  </div>
+                  <div className="col">
+                    <button class="btn btn-primary btn-block">Turbine</button>
+                  </div>
+                  <div className="col">
+                    <button class="btn btn-primary btn-block">FW Pump</button>
+                  </div>
+                </div>
 
-          <div className="row">
-            <div class="col">
-              <button class="btn btn-primary btn-block">Boiler</button>
-            </div>
-            <div class="col">
-              <button class="btn btn-primary btn-block">Turbine</button>
-            </div>
-            <div class="col">
-              <button class="btn btn-primary btn-block">FW Pump</button>
-            </div>
-          </div>
+                <div className="row">
+                  <div className="col">
+                    <button className="btn btn-primary btn-block">Boiler</button>
+                  </div>
+                  <div className="col">
+                    <button className="btn btn-primary btn-block">Turbine</button>
+                  </div>
+                  <div className="col">
+                    <button className="btn btn-primary btn-block">FW Pump</button>
+                  </div>
+                </div>
 
-          <div className="row">
-            <div class="col">
-              <button class="btn btn-primary btn-block">Boiler</button>
+                <div className="row">
+                  <div className="col">
+                    <button className="btn btn-primary btn-block">Boiler</button>
+                  </div>
+                  <div className="col">
+                    <button className="btn btn-primary btn-block">Turbine</button>
+                  </div>
+                  <div className="col">
+                    <button className="btn btn-primary btn-block">FW Pump</button>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col">
+                    <button className="btn btn-primary btn-block">Boiler</button>
+                  </div>
+                  <div className="col">
+                    <button class="btn btn-primary btn-block">Turbine</button>
+                  </div>
+                  <div className="col">
+                    <button className="btn btn-primary btn-block">FW Pump</button>
+                  </div>
+                </div>
+
+              </div>
             </div>
-            <div class="col">
-              <button class="btn btn-primary btn-block">Turbine</button>
+
+            <div className="col">
+              <div className="overview-videogramm">
+
+              </div>
             </div>
-            <div class="col">
-              <button class="btn btn-primary btn-block">FW Pump</button>
+
+            <div className="col-7 alarms">
+              <div className="row">
+                <div className="col-2">
+                  <button className="btn btn-primary btn-block">FW Pump</button>
+                </div>
+              </div>
+
+              <div className="row first-alarm">
+              </div>
+
+              <div className="row">
+              </div>
             </div>
-          </div>
+
+        </div>
 
         </div>
       </div>
